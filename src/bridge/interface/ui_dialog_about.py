80a76c665017f3a31b187f8e7c91a48776b16fda
tutosrive <tutosrivegamer@gmail.com>
2025-06-28 17:26:44 -0500

    Update: Optimize imports, remove unused imports

--- src/bridge/interface/ui_dialog_about.py
+++ src/bridge/interface/ui_dialog_about.py
@@ -8,16 +8,9 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
-    QFormLayout, QGridLayout, QLabel, QScrollArea,
-    QSizePolicy, QWidget)
+from PySide6.QtCore import (QCoreApplication, QLocale, QMetaObject, QRect, QSize, Qt)
+from PySide6.QtGui import QFont
+from PySide6.QtWidgets import (QDialogButtonBox, QFormLayout, QGridLayout, QLabel, QScrollArea, QWidget)
 
 class Ui_about_dialog(object):
     def setupUi(self, about_dialog):
--- src/bridge/interface/ui_main_window.py
+++ src/bridge/interface/ui_main_window.py
@@ -8,17 +8,10 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
-    QCursor, QFont, QFontDatabase, QGradient,
-    QIcon, QImage, QKeySequence, QLinearGradient,
-    QPainter, QPalette, QPixmap, QRadialGradient,
-    QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QLabel,
-    QLayout, QMainWindow, QMenu, QMenuBar,
-    QPushButton, QSizePolicy, QTextEdit, QWidget)
+from PySide6.QtCore import (QCoreApplication, QLocale, QMetaObject, QRect, QSize, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QCursor, QFont, QIcon, QPalette)
+from PySide6.QtWidgets import (QFrame, QGridLayout, QLabel,
+    QLayout, QMainWindow, QMenu, QMenuBar, QPushButton, QSizePolicy, QTextEdit, QWidget)
 from . import resources_rc
 
 class Ui_MainWindow(object):
