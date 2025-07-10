0f0832bbd878ec332f8bf691d7b39480ab0d0d04
tutosrive <tutosrivegamer@gmail.com>
2025-07-09 20:26:51 -0500

    Update dev version in files

--- src/bridgex/__init__.py
+++ src/bridgex/__init__.py
@@ -3,7 +3,7 @@ from .logic.Converter import Converter
 from ._bridgex import Bridgex
 
 __author__:str = "Tutos Rive"
-__version__:str = "0.0.1-dev"
+__version__:str = "0.0.3-dev"
 __license__:str = "MIT"
-__description__:str = "Bridgex is a PySide6 application for file management and conversion, featuring a lite markdown editor and viewer."
+__description__:str = "Graphical interface for converting files to Markdown, built in Python and based on PySide6 and Markitdown."
 __all__:list[str] = ['FileManager', 'Converter', 'Bridgex', '__author__', '__version__', '__license__', '__description__']
