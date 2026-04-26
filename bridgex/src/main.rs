61939972b4c0174efae9012f6591f9e6d07dae03
tutosrive <tutosrivegamer@gmail.com>
2026-04-26 00:38:42 -0500

    Testing slint, first test

---
+++ bridgex/src/main.rs
@@ -0,0 +1,39 @@
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::{ error::Error };
+use rfd::FileDialog;
+use slint::{ SharedString, ToSharedString };
+
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.on_request_file_open({
+        let file = FileDialog::new().add_filter("Documents", &["pdf".to_string()]).pick_file();
+        let mut filename = SharedString::new();
+
+        if let Some(path) = file {
+            filename = path.to_str().unwrap().to_shared_string();
+        }
+
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_filename(ui.get_filename() + filename.as_str())
+        }
+    });
+
+    ui.run()?;
+
+    Ok(())
+}
