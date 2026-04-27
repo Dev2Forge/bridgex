ac03e753f394c9e12ba8e53f238c612b7c2cdd71
tutosrive <tutosrivegamer@gmail.com>
2026-04-27 15:21:51 -0500

    Functional loading license into UI, and functional image in about

--- bridgex/src/main.rs
+++ bridgex/src/main.rs
@@ -8,7 +8,7 @@
  * File: \main.rs
  * Created: Saturday, 25th April 2026 11:34:53 pm
  * -----
- * Last Modified: Monday, 27th April 2026 12:47:09 am
+ * Last Modified: Monday, 27th April 2026 3:19:20 pm
  * Modified By: tutosrive (tutosriveorg@gmail.com)
  * -----
  */
@@ -16,17 +16,21 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{ error::Error };
+use std::{ error::Error, fs, path::PathBuf };
 use rfd::FileDialog;
 use slint::{ SharedString, ToSharedString };
 
 slint::include_modules!();
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
+    let ui_weak = ui.as_weak();
 
     ui.on_request_file_open({
-        let ui_handle = ui.as_weak();
+        let weak = ui_weak.clone();
+
         move || {
+            let ui = weak.upgrade().unwrap();
+
             let file = FileDialog::new().add_filter("Documents", &["pdf".to_string()]).pick_file();
             let mut filename = SharedString::new();
 
@@ -34,19 +38,33 @@ fn main() -> Result<(), Box<dyn Error>> {
                 filename = path.to_str().unwrap().to_shared_string();
             }
 
-            let ui = ui_handle.unwrap();
             ui.set_filename(ui.get_filename() + filename.as_str())
         }
     });
 
-    ui.as_weak();
+    ui.on_request_license_content({
+        let weak = ui_weak.clone();
 
-    let mut content = SharedString::new();
-    content = "Test Content".to_shared_string();
-    ui.global::<LicenseDialogData>().set_content(Into::into(content));
-    // ui.set_license_content(ui.get_license_content() + &"tesst");
+        move || {
+            let ui = weak.upgrade().unwrap();
+
+            let license_name = ui.global::<LibrariesData>().get_library_name();
+
+            let content = load_license_file(license_name.as_str()).unwrap();
+
+            ui.global::<LicenseDialogData>().set_content(Into::into(content))
+        }
+    });
 
     ui.run()?;
 
     Ok(())
+}
+
+fn load_license_file(name: &str) -> Result<SharedString, Box<dyn Error>> {
+    let real_name = if name == "Bridgex" { "gnu3".to_string() } else { name.to_lowercase() };
+    let path: PathBuf = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("src").join("assets").join("licenses").join(format!("license_{}.txt", real_name));
+
+    let readed = fs::read_to_string(path)?;
+    Ok(readed.to_shared_string())
 }
